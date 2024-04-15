3753b3191ebf82813cfa92f917bcb03fa602c1a5
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-15 18:32:03 -0300

    refactor: Altera estrutura de modelos de usuário
    -As classes vao permitir lidar com entrada, saida e att de dados do usuário

--- server/app/models/user_models.py
+++ server/app/models/user_models.py
@@ -5,7 +5,19 @@ Este módulo define os modelos de dados utilizados para representar usuários no
 from pydantic import BaseModel, EmailStr
 
 class UserBaseModel(BaseModel):
-    user_id: int
     username: str
     email: EmailStr
+
+
+class UserInModel(UserBaseModel):
     password: str
+
+
+class UserOutModel(UserBaseModel):
+    user_id: int
+
+
+class UserUpdateModel(BaseModel):
+    username: str
+
+
