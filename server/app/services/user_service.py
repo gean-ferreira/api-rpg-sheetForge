fd79d382b3b55d6801c6987d86888dded50ede6f
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-16 10:00:27 -0300

    feat: Converte os dados da listagem de usuário para o formato UserOutModel

--- server/app/services/user_service.py
+++ server/app/services/user_service.py
@@ -1,9 +1,16 @@
 """ 
 Este módulo contém a lógica de serviço para operações relacionadas aos usuários
 """
+from app.models.user_models import UserOutModel
 from app.repositories.user_repository import UserRepository
 
 class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
+
+
+    async def get_users(self):
+        users_records = await self.user_repository.get_users()
+        users = [UserOutModel(**user) for user in users_records]
+        return users
 
