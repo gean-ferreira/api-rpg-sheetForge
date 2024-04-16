d5d6c04cd434d8bf94ee056cb4a139a6a6d02dd7
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-16 09:51:44 -0300

    feat: Incorpora a classe UserRepository na classe UserService

--- server/app/services/user_service.py
+++ server/app/services/user_service.py
@@ -1,5 +1,9 @@
 """ 
 Este módulo contém a lógica de serviço para operações relacionadas aos usuários
 """
+from app.repositories.user_repository import UserRepository
 
 class UserService:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
