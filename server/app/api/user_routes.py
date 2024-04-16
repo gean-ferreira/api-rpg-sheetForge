10c1d0b2ef704ac7c785ca2af9ad8eefcbc7c80f
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-16 11:57:44 -0300

    refactor: Att o GET /users/ incluindo detalhes na documentação da API

--- server/app/api/user_routes.py
+++ server/app/api/user_routes.py
@@ -8,7 +8,18 @@ user_service = UserService(UserRepository())
 
 
 @router.get(
-    "/users/"
+    "/users/",
+    response_model=UsersListResponseModel,
+    status_code=status.HTTP_200_OK,
+    summary="Lista Todos os Usuários",
+    description="""
+    Retorna uma lista de todos os usuários cadastrados na plataforma.
+    
+    Cada usuário é retornado com seu ID, nome de usuário e e-mail.
+    Ideal para administradores monitorarem os usuários cadastrados.
+    """,
+    response_description="Uma lista de usuários, incluindo detalhes como ID, nome de usuário e e-mail.",
+    tags=["Usuários"],
 )
 async def get_users():
     users = await user_service.get_users()
