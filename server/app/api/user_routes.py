2efef9ebb299607be9f14e8c4e113cf9b048842f
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-16 11:46:28 -0300

    feat: Add a rota GET /users/ no roteador para listar usuários

--- server/app/api/user_routes.py
+++ server/app/api/user_routes.py
@@ -1,4 +1,17 @@
 from fastapi import APIRouter
+from app.models.user_models import (
+    UsersListResponseModel,
+)
 
 router = APIRouter()
+user_service = UserService(UserRepository())
+
+
+@router.get(
+    "/users/"
+)
+async def get_users():
+    users = await user_service.get_users()
+    return UsersListResponseModel(message="Usuários listados com sucesso", data=users)
+
 
