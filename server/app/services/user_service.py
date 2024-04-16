c0cc643add5649dd8bcc8d53330074058936a618
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-16 15:42:23 -0300

    feat: Add método que verifica a existência de usuário e tratar erro caso não encontrar

--- server/app/services/user_service.py
+++ server/app/services/user_service.py
@@ -1,12 +1,22 @@
 """ 
 Este módulo contém a lógica de serviço para operações relacionadas aos usuários
 """
-from app.models.user_models import UserOutModel
+
+from fastapi import HTTPException, status
 from app.repositories.user_repository import UserRepository
+
 
 class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
+
+    async def _verify_user_exists(self, user_id: int):
+        db_user = await self.user_repository.get_user_by_id(user_id)
+        if db_user is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado"
+            )
+        return db_user
 
 
     async def get_users(self):
