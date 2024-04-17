31cbff98e51090b9f029f3eaaf11e1e473b5fcf1
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-17 13:58:39 -0300

    feat: Add GET /users/{user_id}/ para buscar os dados do usuário

--- server/app/api/user_routes.py
+++ server/app/api/user_routes.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
+from fastapi import APIRouter, status
+from app.models.error_models import DetailErrorResponse, ErrorResponseModel
 from app.models.user_models import (
+    UserOutDataModel,
     UsersListResponseModel,
 )
 
@@ -24,5 +27,37 @@ user_service = UserService(UserRepository())
 async def get_users():
     users = await user_service.get_users()
     return UsersListResponseModel(message="Usuários listados com sucesso", data=users)
+
+
+@router.get(
+    "/users/{user_id}/",
+    responses={
+        200: {
+            "model": UserOutDataModel,
+            "description": "Os detalhes do usuário, incluindo ID, nome de usuário e e-mail, são retornados na resposta.",
+        },
+        404: {
+            "model": DetailErrorResponse,
+            "description": "Retorna uma mensagem indicando que o usuário não foi encontrado.",
+        },
+        422: {
+            "model": ErrorResponseModel,
+            "description": "Erro de validação na entrada de dados.",
+        },
+    },
+    summary="Busca Dados do Usuário",
+    description="""
+    Busca um usuário específico pela sua identificação única (ID).
+
+    Retorna os dados do usuário, incluindo ID, nome de usuário e e-mail, caso encontrado.
+    Caso contrário, retorna um erro 404 indicando que o usuário não foi encontrado.
+    """,
+    tags=["Usuários"],
+)
+async def read_user_data(user_id: int):
+    db_user = await user_service.get_user_by_id(user_id)
+    return UserOutDataModel(
+        message="Dados do usuário encontrados com sucesso", data=db_user
+    )
 
 
