53d232da3f954751b2923d873010df3cc32f8671
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-17 14:34:46 -0300

    feat: Integra o manipulador de exceções para erros de validaçào

--- server/main.py
+++ server/main.py
@@ -1,10 +1,13 @@
 # Main file
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 from app.core.database import database
 from app.api.user_routes import router as user_router
+from app.exceptions.exception_handlers import validation_exception_handler
 
 app = FastAPI()
 app.include_router(user_router)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 
 @app.on_event("startup")
