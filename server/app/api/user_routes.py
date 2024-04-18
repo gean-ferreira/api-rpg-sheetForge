e1900229144f4a9a6d3d2befb0343c21abb6788b
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-18 10:05:28 -0300

    feat: Add rota POST /users/ para criar novos usuários

--- server/app/api/user_routes.py
+++ server/app/api/user_routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from fastapi import APIRouter, status
 from app.models.error_models import DetailErrorResponse, ErrorResponseModel
+from app.models.response_models import BaseResponseModel
 from app.models.user_models import (
+    UserInModel,
     UserOutDataModel,
     UsersListResponseModel,
 )
@@ -59,5 +61,37 @@ async def read_user_data(user_id: int):
     return UserOutDataModel(
         message="Dados do usuário encontrados com sucesso", data=db_user
     )
+
+
+@router.post(
+    "/users/",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        201: {
+            "model": UserOutDataModel,
+            "description": "Usuário criado com sucesso. Retorna os detalhes do novo usuário, exceto a senha.",
+        },
+        400: {
+            "model": DetailErrorResponse,
+            "description": "Falha na criação do usuário devido à tentativa de uso de um e-mail ou username já existente.",
+        },
+        422: {
+            "model": ErrorResponseModel,
+            "description": "Erro de validação na entrada de dados. Pode ocorrer por dados formatados incorretamente, como um e-mail inválido.",
+        },
+    },
+    summary="Cria um Novo Usuário",
+    description="""
+    Registra um novo usuário na plataforma. 
+
+    Para criar um usuário, é necessário fornecer um nome de usuário, um endereço de e-mail e uma senha. A senha fornecida será criptografada antes de ser armazenada.
+
+    Em caso de sucesso, retorna os dados do usuário criado. Se o nome de usuário ou e-mail já estiverem em uso, um erro será retornado.
+    """,
+    tags=["Usuários"],
+)
+async def create_user(user: UserInModel):
+    db_user = await user_service.create_user(user)
+    return UserOutDataModel(message="Usuário cadastrado com sucesso", data=db_user)
 
 
