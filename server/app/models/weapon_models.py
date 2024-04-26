3e6cf7451ac7d2e36b53c38239a913f4c236130f
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-26 13:40:30 -0300

    feat: Define os modelos de dados para as armas de RPG

---
+++ server/app/models/weapon_models.py
@@ -0,0 +1,33 @@
+""" 
+Este módulo define os modelos de dados para armas utilizadas no sistema de RPG
+"""
+
+from typing import List, Optional
+from pydantic import BaseModel
+from enum import Enum
+
+from app.models.response_models import ResponseWithDataModel
+
+
+class AbilityModifier(Enum):
+    FORÇA = "For"
+    DESTREZA = "Des"
+    INTELIGÊNCIA = "Int"
+
+
+class WeaponType(Enum):
+    ESMAGAMENTO = "Esmagamento"
+    PERFURAÇÃO = "Perfuração"
+    CORTE = "Corte"
+
+
+class WeaponBaseModel(BaseModel):
+    weapon_id: int
+    name: str
+    damage: str
+    critical: str
+    ability_modifier: str
+    attack_range: str
+    damage_type: str
+
+
