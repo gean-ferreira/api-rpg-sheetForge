78f12f124e245e5f8070688ee158137fc953bb75
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-26 13:02:35 -0300

    feat: Cria a classe WeaponRepository para gerenciamentos das operações CRUD

---
+++ server/app/repositories/weapon_repository.py
@@ -0,0 +1,8 @@
+""" 
+Este módulo contém funções que interagem diretamente com o banco de dados para as operações CRUD relacionadas às armas no contexto do RPG
+"""
+
+from app.core.database import database
+
+
+class WeaponRepository:
