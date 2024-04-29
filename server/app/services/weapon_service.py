1f543cce42fbbdf2c195578ae336f55616493201
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-29 12:53:52 -0300

    feat: Implementa o método get_weapons em WeaponService para buscar e converter registros de armas

--- server/app/services/weapon_service.py
+++ server/app/services/weapon_service.py
@@ -3,8 +3,14 @@ Este módulo contém a lógica de serviço para operações relacionadas às arm
 """
 
 from app.repositories.weapon_repository import WeaponRepository
+from app.models.weapon_models import WeaponBaseModel
 
 
 class WeaponService:
     def __init__(self, weapon_repository: WeaponRepository):
         self.weapon_repository = weapon_repository
+
+    async def get_weapons(self):
+        weapons_records = await self.weapon_repository.get_weapons()
+        weapons = [WeaponBaseModel(**weapon) for weapon in weapons_records]
+        return weapons
