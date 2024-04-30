69112a85a753b7554a1254b370397e12ae859c11
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-30 12:17:38 -0300

    feat: Add método que verifica se uma arma existente no banco de dados

--- server/app/repositories/weapon_repository.py
+++ server/app/repositories/weapon_repository.py
@@ -9,3 +9,7 @@ class WeaponRepository:
     async def get_weapons(self):
         query = "SELECT * FROM RPG.Weapons;"
         return await database.fetch_all(query)
+
+    async def get_weapon_by_id(self, weapon_id: int):
+        query = "SELECT * FROM RPG.Weapons WHERE weapon_id = :id;"
+        return await database.fetch_one(query, {"id": weapon_id})
--- server/app/services/weapon_service.py
+++ server/app/services/weapon_service.py
@@ -2,6 +2,7 @@
 Este módulo contém a lógica de serviço para operações relacionadas às armas no contexto do RPGs
 """
 
+from fastapi import HTTPException, status
 from app.repositories.weapon_repository import WeaponRepository
 from app.models.weapon_models import WeaponBaseModel
 
@@ -9,6 +10,10 @@ from app.models.weapon_models import WeaponBaseModel
 class WeaponService:
     def __init__(self, weapon_repository: WeaponRepository):
         self.weapon_repository = weapon_repository
+
+    async def _verify_weapon_exists(self, weapon_id: int):
+        db_weapon = await self.weapon_repository.get_weapon_by_id(weapon_id)
+        return db_weapon
 
     async def get_weapons(self):
         weapons_records = await self.weapon_repository.get_weapons()
