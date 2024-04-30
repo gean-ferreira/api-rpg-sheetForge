5f809f565568b7e3a223a9477e33d5e2da43cdd4
Gean Ferreira <gean_roberto@hotmail.com>
2024-04-30 12:52:26 -0300

    feat: Add GET para encontrar dados detalhados da arma por ID

--- server/app/api/weapon_routes.py
+++ server/app/api/weapon_routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, status
 from app.repositories.weapon_repository import WeaponRepository
 from app.services.weapon_service import WeaponService
-from app.models.weapon_models import WeaponBaseModel, WeaponsListResponseModel
+from app.models.weapon_models import WeaponOutDataModel, WeaponsListResponseModel
+from app.models.error_models import DetailErrorResponse, ErrorResponseModel
 
 router = APIRouter()
 weapon_service = WeaponService(WeaponRepository())
@@ -24,3 +25,35 @@ weapon_service = WeaponService(WeaponRepository())
 async def get_weapons():
     weapons = await weapon_service.get_weapons()
     return WeaponsListResponseModel(message="Armas listados com sucesso", data=weapons)
+
+
+@router.get(
+    "/weapons/{weapon_id}/",
+    responses={
+        200: {
+            "model": WeaponOutDataModel,
+            "description": "Retorna os detalhes completos da arma, incluindo ID, nome, tipo de dano, e mais.",
+        },
+        404: {
+            "model": DetailErrorResponse,
+            "description": "Retorna uma mensagem indicando que a arma não foi encontrada.",
+        },
+        422: {
+            "model": ErrorResponseModel,
+            "description": "Erro de validação na entrada de dados.",
+        },
+    },
+    summary="Busca Dados da Arma",
+    description="""
+    Busca uma arma específica pelo seu ID.
+
+    Retorna os detalhes completos da arma, incluindo ID, nome, tipo de dano, e outros atributos relevantes.
+    Se a arma não for encontrada, retorna um erro 404.
+    """,
+    tags=["Armas"],
+)
+async def read_weapon_data(weapon_id: int):
+    db_weapon = await weapon_service.get_weapon_by_id(weapon_id)
+    return WeaponOutDataModel(
+        message="Dados da arma encontrados com sucesso", data=db_weapon
+    )
