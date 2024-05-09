38dfc351dd758bac5608e8c1b3938592ba594a9e
Gean Ferreira <gean_roberto@hotmail.com>
2024-05-09 09:17:26 -0300

    feat: Implementa funcão que verifica/compara senhas utilizando bcrypt

--- server/app/security/security.py
+++ server/app/security/security.py
@@ -1,4 +1,5 @@
 from passlib.context import CryptContext
+import bcrypt
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -6,3 +7,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Criptografa a senha
 async def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+
+# Compara senha com a senha criptografada
+def verify_password(plain_password, hashed_password):
+    return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
