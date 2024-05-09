c86cb64a9e1e0f82202199a6d231bfded487d4b4
Gean Ferreira <gean_roberto@hotmail.com>
2024-05-09 10:28:18 -0300

    feat: Add funcionalidade para definir cookies HTTP para maior seguraça

--- server/app/api/auth_routes.py
+++ server/app/api/auth_routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime
+from fastapi import APIRouter, Depends, Response
 from app.repositories.user_repository import UserRepository
 from app.services.auth_service import AuthService
 from app.models.auth_models import LoginFormData
@@ -7,10 +8,18 @@ router = APIRouter()
 auth_service = AuthService(UserRepository())
 
 
-@router.post("/auth/login/")
-async def login(form_data: LoginFormData = Depends()):
+async def login(response: Response, form_data: LoginFormData = Depends()):
     access_token, expire = await auth_service.authenticate_and_generate_token(
         form_data.username, form_data.password
+    )
+    max_age = int((expire - datetime.now()).total_seconds())
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        max_age=max_age,
+        secure=True,
+        samesite="Strict",
     )
     return ResponseWithDataModel(
         message="Login efetuado com sucesso",
